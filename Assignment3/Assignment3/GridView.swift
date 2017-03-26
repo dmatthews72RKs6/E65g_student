414f560bd222dfa11fef44b316456ea7c81d7060
David Matthews <dmatthews@g.havard.edu>
2017-03-26 12:24:00 -0400

    dynamically draws grid, can draw a circle. next need to go through the Grid object and draw every circle as the correct color

--- Assignment3/Assignment3/GridView.swift
+++ Assignment3/Assignment3/GridView.swift
@@ -28,7 +28,60 @@ import UIKit
     
 
     override func draw(_ rect: CGRect) {
+        let cellSize = CGSize (
+            width: rect.size.width / CGFloat(size),
+            height: rect.size.height / CGFloat(size)
+        )
 
+        
+        // draw the grid
+        
+        (0...size).forEach {
+            // horizontal
+            drawLine(
+                start:  CGPoint(x: 0.0, y: CGFloat($0)/CGFloat(size) * rect.size.height),
+                end:    CGPoint(x: rect.size.width, y: CGFloat($0)/CGFloat(size) * rect.size.height)
+            )
+            
+            // verticle
+            drawLine(
+                start:  CGPoint(x: CGFloat($0)/CGFloat(size) * rect.size.width, y: 0.0),
+                end:    CGPoint(x: CGFloat($0)/CGFloat(size) * rect.size.width, y: rect.size.height)
+            )
+        }
+
+        // draw the circles
+        
+        drawCircle(origin: CGPoint(x: 0, y: 0), size: cellSize, color: UIColor.brown)
+        
+    }
+    
+    /**
+    Draws a line of a desired color from start to end.
+     
+     - Parameter start: The point to start drawing the line at.
+     - Parameter end: The point to end the line at.
+     - Parameter color: The color of the line.
+    */
+    func drawLine (start: CGPoint, end: CGPoint) {
+        let path = UIBezierPath()
+        path.lineWidth = gridWidth
+        path.move(to: start)
+        path.addLine(to: end)
+        UIColor.black.setStroke()
+        path.stroke()
+    }
+    
+    //TODO: make the circles slightly smaller to not overlap with the grid lines
+    
+    func drawCircle (origin: CGPoint, size: CGSize, color: UIColor) {
+        let rect = CGRect(
+            origin: origin,
+            size: size
+        )
+        let path = UIBezierPath(ovalIn: rect)
+        color.setFill()
+        path.fill()
     }
 
 }
