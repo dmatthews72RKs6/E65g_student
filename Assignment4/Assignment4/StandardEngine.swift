50b1d1fc569e3f8872b375addb6f9e3a6e31c1ed
David Matthews <dmatthews@g.havard.edu>
2017-04-24 11:47:34 -0400

    working on part 3

---
+++ Assignment4/Assignment4/StandardEngine.swift
@@ -0,0 +1,67 @@
+//
+//  StandardEngine.swift
+//  Assignment4
+//
+//  Created by Water on 4/19/17.
+//  Copyright © 2017 Harvard Division of Continuing Education. All rights reserved.
+//
+
+import Foundation
+public protocol EngineProtocol {
+    var delegate: EngineDelegate { get set }
+    var grid: GridProtocol { get }
+    var refreshRate: Double { get set }
+    var refreshTimer: Timer { get set }
+    var size: Int { get set}
+    init (rows: Int, cols: Int)
+    func step () -> GridProtocol
+}
+
+public class StandardEngine: EngineProtocol {
+    public var size: Int = 10
+    public var grid: GridProtocol
+    
+    public var refreshRate: Double = 0
+    public var refreshTimer: Timer
+    
+
+    static var engine: StandardEngine = StandardEngine(rows: 10, cols: 10)
+    
+    
+    var updateClosure: ((Grid) -> Void)?
+    var timer: Timer?
+    var timerInterval: TimeInterval = 0.0 {
+        didSet {
+            if timerInterval > 0.0 {
+                timer = Timer.scheduledTimer(
+                    withTimeInterval: timerInterval,
+                    repeats: true
+                ) { (t: Timer) in
+                    _ = self.step()
+                }
+            }
+            else {
+                timer?.invalidate()
+                timer = nil
+            }
+        }
+    }
+        
+        public init(rows: Int, cols: Int) {
+            self.grid = Grid(rows, cols)
+        }
+        
+        func step() -> GridProtocol {
+            let newGrid = grid.next()
+            grid = newGrid
+            //         updateClosure?(self.grid)
+            delegate?.engineDidUpdate(withGrid: grid)
+                      let nc = NotificationCenter.default
+                      let name = Notification.Name(rawValue: "EngineUpdate")
+                      let n = Notification(name: name,
+                                           object: nil,
+                                           userInfo: ["engine" : self])
+                                           nc.post(n)
+            return grid
+        }
+}
